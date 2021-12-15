1057d3eb544afe4841a6650a629ec480b173e712
RajiAmma <93925621+RajiAmma@users.noreply.github.com>
2021-12-15 19:00:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload(){}
+function setup(){
+    canvas=createCanvas(300,300);
+    //canvas.center();
+    canvas.position(550,200);
+}
+function draw(){}
+function take_snapshot(){
+    save('snapshot.png');
+}
